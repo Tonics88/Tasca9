b4ad09513a6199d14b6dfdb81c28e6ef0bfe91f2
Tonics88 <tcs1988@gmail.com>
2018-01-25 15:46:02 +0100

    Calcul mensualitat realitzat

--- js/calcularQuota.js
+++ js/calcularQuota.js
@@ -1,6 +1,36 @@
-function calcularAnys(val) {
+hipoteca={
+    inici:0,
+    capital: 0,
+    interes: 0,
+    termini: 0,
+    cuota: function() {
+        return ((this.capital*this.interes/12)/(100*(1-Math.pow(1+this.interes/(12*100),this.termini*-1)))).toFixed(2);
+    }
+}
+
+function calcularInici(val) {
+
+}
+
+function calcularCapital(val) {
+    hipoteca.capital=val;
+}
+
+function calcularInteres(val) {
+    hipoteca.interes=val;
+}
+
+function calcularTermini(val) {
+    hipoteca.termini=val;
     document.getElementById("mesosAnys").innerHTML = (val)+" mesos seran "+parseInt(val/12)+" anys";
     if (val%12){
         document.getElementById("mesosAnys").innerHTML += " i "+(val%12)+" mesos"
     } 
+    resultatCuota();
+}
+
+function resultatCuota (){
+    if (parseFloat(hipoteca.cuota())>0){
+        document.getElementById("resultat").innerHTML = " "+hipoteca.cuota()+" €";
+    }
 }
