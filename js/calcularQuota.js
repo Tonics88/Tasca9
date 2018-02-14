0fbbf7a52486e4ac89dd5f6fe3c0d6fcf01d3e31
Antonio Caballero Salas <toni.caballero@sm2baleares.es>
2018-02-14 17:03:02 +0100

    Afegida predicció data fi hipoteca

--- js/calcularQuota.js
+++ js/calcularQuota.js
@@ -1,5 +1,6 @@
 hipoteca={
-    inici:0,
+    mes:0,
+    any:0,
     capital: 0,
     interes: 0,
     termini: 0,
@@ -9,7 +10,35 @@ hipoteca={
 }
 
 function calcularInici(val) {
-    hipoteca.inici=val;
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //Gener es el 0
+    console.log(typeof(dd))
+    console.log(dd);
+    
+    hipoteca.mes=parseInt(val.substr(5));
+    hipoteca.any=parseInt(val.substr(0, 4));
+    
+    if (dd!=1 && hipoteca.mes==mm){
+        if (hipoteca.mes==12){
+            hipoteca.any++;
+        }
+        hipoteca.mes=(hipoteca.mes%12+1);
+        if(hipoteca.mes<10) {
+            var mm = '0'+hipoteca.mes;
+        }
+        console.log(mm);
+        document.getElementById("inici").value=(hipoteca.any+'-'+mm);
+    }
+    console.log(typeof(hipoteca.mes));
+    console.log(hipoteca.mes);
+    console.log(typeof(hipoteca.any));
+    console.log(hipoteca.any);
+    
+    
+    
+    
+    console.log(val);
     resultatCuota();
 }
 
@@ -33,7 +62,7 @@ function calcularTermini(val) {
     document.getElementById("mesosAnys").innerHTML = (val)+" mesos seran "+parseInt(val/12)+" anys";
     if (val%12){
         document.getElementById("mesosAnys").innerHTML += " i "+(val%12)+" mesos"
-    } 
+    }
     resultatCuota();
 }
 
@@ -41,4 +70,5 @@ function resultatCuota (){
     if (parseFloat(hipoteca.cuota())>0 && hipoteca.cuota()!="Infinity"){
         document.getElementById("resultat").innerHTML = " "+hipoteca.cuota()+" €";
     }
+    document.getElementById("final").innerHTML = " Mes "+(hipoteca.mes+parseInt(hipoteca.termini))%12+" del "+(hipoteca.any+parseInt(hipoteca.termini/12));
 }
