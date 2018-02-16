d9baa9edddd58dccbb5c19b78b4a1f37da2e751a
Antonio Caballero Salas <toni.caballero@sm2baleares.es>
2018-02-16 16:28:04 +0100

    complet a falta de comentar i revisar

--- js/calcularQuota.js
+++ js/calcularQuota.js
@@ -3,18 +3,17 @@ hipoteca={
     any:0,
     capital: 0,
     interes: 0,
-    termini: 0,
+    termini: 1,
     cuota: function() {
         return ((this.capital*this.interes/12)/(100*(1-Math.pow(1+this.interes/(12*100),this.termini*-1)))).toFixed(2);
-    }
+    },
+    avis:true
 }
 
 function calcularInici(val) {
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth()+1; //Gener es el 0
-    console.log(typeof(dd))
-    console.log(dd);
     
     hipoteca.mes=parseInt(val.substr(5));
     hipoteca.any=parseInt(val.substr(0, 4));
@@ -30,31 +29,58 @@ function calcularInici(val) {
         console.log(mm);
         document.getElementById("inici").value=(hipoteca.any+'-'+mm);
     }
-    console.log(typeof(hipoteca.mes));
-    console.log(hipoteca.mes);
-    console.log(typeof(hipoteca.any));
-    console.log(hipoteca.any);
     
-    
-    
-    
-    console.log(val);
     resultatCuota();
 }
 
 function calcularCapital(val) {
-    if (val<=1000000){
+    
+    if (val>0 && val<=1000000){
         hipoteca.capital=val;
+        valOk("capital");
         resultatCuota();
     } else {
         
+        if (val>1000000){
+            valNoOk("capital");
+            document.getElementById("capital").value=1000000;
+            if (hipoteca.avis){
+                hipoteca.avis=false;
+                setTimeout(maxCapital,100);
+                
+            }
+        } else {
+            document.getElementById("capital").value="";
+            hipoteca.capital=0;
+        }
     }
     
 }
 
+function maxCapital(){
+    
+    if (confirm("El capital no pot ser superior a 1 milió d'euros.\nVols un milió?")){
+        document.getElementById("capital").value=1000000;
+        hipoteca.capital=1000000;
+        valOk("capital");
+    } else {
+        document.getElementById("capital").value="";
+        hipoteca.capital=0;
+    }
+    hipoteca.avis=true;
+    
+}
+
 function calcularInteres(val) {
-    hipoteca.interes=val;
-    resultatCuota();
+    if (parseFloat(val)>=0){
+        hipoteca.interes=val;
+        valOk("interes");
+        resultatCuota();
+    } else {
+        valNoOk("interes");
+        hipoteca.interes=0;
+        document.getElementById("interes").value="";
+    }
 }
 
 function calcularTermini(val) {
@@ -70,5 +96,19 @@ function resultatCuota (){
     if (parseFloat(hipoteca.cuota())>0 && hipoteca.cuota()!="Infinity"){
         document.getElementById("resultat").innerHTML = " "+hipoteca.cuota()+" €";
     }
-    document.getElementById("final").innerHTML = " Mes "+(parseInt(hipoteca.termini)%12+hipoteca.mes)+" del "+(hipoteca.any+parseInt(hipoteca.termini/12));
+    var mes=(hipoteca.mes+((hipoteca.termini-1)%12))%12;
+    if (!mes){
+        mes=12;
+    }
+    document.getElementById("final").innerHTML = " Mes "+mes+" del "+(hipoteca.any+parseInt(hipoteca.termini/12));
+}
+
+function valNoOk(text){
+    document.getElementById(text).setAttribute("class", "form-control is-invalid");
+    document.getElementById(text+"_text").setAttribute("class", "invalid-feedback");
+}
+
+function valOk(text){
+    document.getElementById(text).setAttribute("class", "form-control is-valid");
+    document.getElementById(text+"_text").setAttribute("class", "form-text text-muted");
 }
